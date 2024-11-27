db4dfa9a117c1b76d58bc0d60a1956c05d1746b1
Adeel-116 <adeel8128377@gmail.com>
2024-11-27 05:02:20 +0500

    Splash Screen Develop

---
+++ GymProject/App.tsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { View } from 'react-native';
+import LoadingScreen from './Components/LoadingScreen';
+
+const App = () => {
+  return (
+      <View>
+        <LoadingScreen />
+      </View>   
+  );
+};
+
+
+
+export default App;
